055fff28484d94165a2ed3f60acbc343c91075da
Exzzy <i.reizeee@gmail.com>
2026-08-27 15:07:56 +0700

    Добавлено функциональное меню

--- app/tracker.py
+++ app/tracker.py
@@ -29,4 +29,36 @@ class ExpenseTracker:
     def filter_by_category(self, category):
         return [expense for expense in self.expenses if expense.category == category]
 
+
+def main():
+    tracker = ExpenseTracker()
+ 
+    while True:
+        print('=== Expense Tracker ===\n1. Добавить расход\n2. Показать все расходы\n3. Показать общую сумму расходов\n4. Показать расходы по категории\n5. Выйти')
+        choice = input('Выберите действие: ')
     
+        if int(choice) == 1:
+            name = input('Название расхода: ')
+            amount = int(input('Сумма: '))
+            category = input('Категория: ')
+            tracker.add_expense(name, amount, category)
+    
+        elif int(choice) == 2:
+            expenses_list = tracker.get_all_expenses()
+            for index, expense in enumerate(expenses_list, start=1):
+                print(f"{index}. {expense}")
+    
+        elif int(choice) == 3:
+            print(f'Total: {tracker.get_total_expenses()}')
+    
+        elif int(choice) == 4:
+            category = input('Какая категория? ')
+            found = tracker.filter_by_category(category)
+            for expense in found:
+                print(f"{expense.name} - {expense.amount}")
+    
+        elif int(choice) == 5:
+            break
+
+if __name__ == "__main__":
+    main()        
